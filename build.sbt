a0167ac24f36b864ed04a91d634749d1d6837adc
Hannes Widmoser <widmoser@gmail.com>
2017-03-18 13:34:34 +0100

    Setup project

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "durooma-api"
+
+version := "0.1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.4"
+    
---
+++ src/main/scala/WebServer.scala
@@ -0,0 +1,36 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+import scala.io.StdIn
+
+/**
+  * Created by hannes on 18/03/2017.
+  */
+object WebServer {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("requests");
+    implicit val materializer = ActorMaterializer();
+    implicit val executionContext = system.dispatcher;
+
+    val route = path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+
+  }
+
+}
