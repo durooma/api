cbc19975d49b41551229f765008a956b4ae65573
Hannes Widmoser <widmoser@gmail.com>
2017-03-24 23:49:44 +0100

    Add error handler for duplicate entries

--- src/main/scala/com/durooma/api/route/Api.scala
+++ src/main/scala/com/durooma/api/route/Api.scala
@@ -3,6 +3,7 @@ package com.durooma.api.route
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.ExceptionHandler
 import com.durooma.api.error.AuthenticationError
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException
 
 /**
   * Created by hannes on 18/03/2017.
@@ -11,6 +12,12 @@ object Api extends CustomDirectives with JsonSupport {
 
   val genericExceptionHandler = ExceptionHandler {
     case AuthenticationError(message) => complete((StatusCodes.Unauthorized, message))
+    case e: MySQLIntegrityConstraintViolationException => e.getErrorCode match {
+      case 1062 => complete((StatusCodes.BadRequest, e.getMessage))
+    }
+    case e =>
+      e.printStackTrace()
+      complete((StatusCodes.InternalServerError, e.getLocalizedMessage))
   }
 
   val route = handleExceptions(genericExceptionHandler) {
